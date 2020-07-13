886a065037020dc4930a746562bf08cb4598da7a
Emil Litwiniec <emilitwiniec@gmail.com>
2020-07-14 00:19:21 +0200

    Initial login component structure with base styling

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,13 +1,36 @@
 <template>
-	<div class="login-box">
-
+	<div ref="login-box" class="login__box" :class="`login__box--${progress}`">
+		<button @click="onSignInClicked" class="login__btn">Sign In</button>
+		<button @click="onSignUpClicked" class="login__btn">Sign Up</button>
 	</div>
 </template>
 
-<script>
-import { Component, Prop, Vue } from "vue-property-decorator";
+<script lang="ts">
+import { Component, Prop, Vue, Ref, Watch } from "vue-property-decorator";
+import { gsap } from "gsap";
 
-@Component()
-export default class Login extends Vue {}
+enum loginProgress {
+	IDLE = "idle",
+	SIGN_IN = "sign-in",
+	SIGN_UP = "sign-up"
+}
 
+@Component
+export default class Login extends Vue {
+	private progress: loginProgress = loginProgress.IDLE;
+	@Ref("login-box") readonly loginBox: any;
+
+	onSignInClicked() {
+		this.progress = loginProgress.SIGN_IN;
+	}
+
+	onSignUpClicked() {
+		this.progress = loginProgress.SIGN_UP;
+	}
+
+	@Watch("progress")
+	onProgressChange(value: loginProgress) {
+		return;
+	}
+}
 </script>
